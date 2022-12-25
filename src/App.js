88c9e516292d0008d11731b516cbfff56a33f8ab
Hussein Aassem <hussienasem+gitlab@gmail.com>
2022-12-25 16:48:58 +0200

    added events

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
+
+const EVENTS_TO_TEST = [
+  // mouse events
+  "click",
+  "contextmenu",
+  "dblclick",
+  "mousedown",
+  "mouseenter",
+  "mouseleave",
+  "mousemove",
+  "mouseout",
+  "mouseover",
+  "mouseup",
+  "wheel",
+
+  // touch events
+  "touchcancel",
+  "touchend",
+  "touchmove",
+  "touchstart",
+
+  // scroll
+  "scroll",
+
+  // drag
+  "drag",
+  "dragend",
+  "dragenter",
+  "dragleave",
+  "dragover",
+  "dragstart",
+  "drop",
+];
 
 function App() {
+  const [firedEvents, setFiredEvents] = useState([]);
+  useEffect(() => {
+    const listeners = EVENTS_TO_TEST.map((ev) => ({
+      event: ev,
+      listener: () => setFiredEvents((events) => [...events, ev]),
+    }));
+    listeners.forEach((listener) => {
+      window.addEventListener(listener.event, listener.listener);
+    });
+
+    return () => {
+      listeners.forEach((listener) => {
+        window.removeEventListener(listener.event, listener.listener);
+      });
+    };
+  }, []);
+
+  const firedEventsSet = new Set(firedEvents);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        {[...firedEventsSet].map((ev) => (
+          <li key={ev}>{ev}</li>
+        ))}
+      </ul>
     </div>
   );
 }
